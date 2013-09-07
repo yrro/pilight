ea1c44aae27f071c13519d72d88898826f822f4f
Sam Morris <sam@robots.org.uk>
2013-09-07 17:43:37 +0100

    Add saw anim function

--- anims.py
+++ anims.py
@@ -1,4 +1,11 @@
 import math
 
+def saw (t):
+        q, r = divmod (t, 1)
+        if r < 0.5:
+                return 2*r
+        else:
+                return 1-2*(r-0.5)
+
 def sine (t):
         return (1 + math.sin (t))/2
